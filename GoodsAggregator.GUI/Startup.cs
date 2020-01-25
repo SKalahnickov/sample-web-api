32396effe63a739ddf8babf17fa65408e13b65aa
Sergey <svkalashnickov@gmail.com>
2020-01-25 13:49:54 +0300

    Configured migrations

--- GoodsAggregator.GUI/Startup.cs
+++ GoodsAggregator.GUI/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GoodsAggregator.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +20,9 @@ namespace GoodsAggregator.GUI
 		public Startup(IConfiguration configuration)
 		{
 			Configuration = configuration;
+
+			using var context = new BaseContext();
+			context.Database.Migrate();
 		}
 
 		public IConfiguration Configuration { get; }
