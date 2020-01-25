63010c0362dace2e51a9f1ab59ceef4c95225311
Sergey <svkalashnickov@gmail.com>
2020-01-25 13:52:14 +0300

    [naming issue]

--- GoodsAggregator.Partsers/DAL/ParserContext.cs
+++ GoodsAggregator.Partsers/DAL/ParserContext.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace GoodsAggregator.Partsers.DAL
+namespace GoodsAggregator.Parsers.DAL
 {
 	public class ParserContext : BaseContext
 	{
