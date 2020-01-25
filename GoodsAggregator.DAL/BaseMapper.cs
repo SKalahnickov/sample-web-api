3e71321e05ccac71ef26a0d02afccfef32685190
Sergey <svkalashnickov@gmail.com>
2020-01-25 13:47:39 +0300

    Added base mapper

---
+++ GoodsAggregator.DAL/BaseMapper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodsAggregator.DAL
+{
+	public interface IBaseMapper
+	{
+		void Map(ModelBuilder modelBuilder);
+	}
+
+	public abstract class BaseMapper<TEntity> : IBaseMapper
+		where TEntity : class
+	{
+		public void Map(ModelBuilder modelBuilder)
+			=> Map(modelBuilder.Entity<TEntity>());
+
+		public abstract void Map(EntityTypeBuilder<TEntity> entity);
+	}
+}
