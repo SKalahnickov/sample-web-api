6ce3af1dbb9e09700337b84d2a618098592e935b
Sergey <svkalashnickov@gmail.com>
2020-01-25 13:48:00 +0300

    Added added configured base context

---
+++ GoodsAggregator.DAL/BaseContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace GoodsAggregator.DAL
+{
+	public class BaseContext : DbContext
+	{
+		public BaseContext()
+		{
+			Database.EnsureCreated();
+		}
+
+		protected sealed override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseNpgsql(@"Server=localhost;Port=5432;Database=GoodsAggregator;UserName=postgres;Password=123");
+		}
+
+		protected sealed override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			var assembly = typeof(IBaseMapper).Assembly;
+
+			var mappers = assembly.GetTypes()
+				.Where(value => !value.IsAbstract)
+				.Where(value => !value.IsInterface)
+				.Where(value => !value.IsGenericType)
+				.Where(value => !value.IsNested)
+				.Where(value => typeof(IBaseMapper).IsAssignableFrom(value))
+				.Select(value => Activator.CreateInstance(value) as IBaseMapper);
+
+			foreach (var mapper in mappers)
+				mapper.Map(modelBuilder);
+		}
+	}
+}
