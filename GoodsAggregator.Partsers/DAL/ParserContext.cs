fd66afb979ddf221fdf5ef04181be9d4827411ee
Sergey <svkalashnickov@gmail.com>
2020-01-25 13:49:33 +0300

    Implemented parser context

---
+++ GoodsAggregator.Partsers/DAL/ParserContext.cs
@@ -0,0 +1,18 @@
+﻿using GoodsAggregator.DAL;
+using GoodsAggregator.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodsAggregator.Partsers.DAL
+{
+	public class ParserContext : BaseContext
+	{
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Good> Goods { get; set; }
+		public DbSet<GoodInCategory> GoodsInCategories { get; set; }
+		public DbSet<Merchant> Merchants { get; set; }
+		public DbSet<PriceDynamics> Prices { get; set; }
+	}
+}
